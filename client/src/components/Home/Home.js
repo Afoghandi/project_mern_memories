60313f5bd2a59990eea55226f06cf891f7c8a80b
Afo Oshun <afoyenyen@Afos-iMac.local>
2021-04-19 15:02:32 +0100

    part 3

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Container, Grid, Grow } from '@material-ui/core';
+import Posts from '../Posts/Posts';
+import Form from '../Form/Form';
+import { getPosts } from '../../actions/posts';
+
+const Home = () => {
+	const [currentId, setCurrentId] = useState(0);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getPosts());
+	}, [currentId, dispatch]);
+	return (
+		<div>
+			<Container>
+				<Grid
+					container
+					justify='space-between'
+					alignItems='stretch'
+					spacing={3}
+				>
+					<Grid item xs={12} sm={7}>
+						<Posts setCurrentId={setCurrentId} />{' '}
+					</Grid>{' '}
+					<Grid item xs={12} sm={4}>
+						<Form currentId={currentId} setCurrentId={setCurrentId} />{' '}
+					</Grid>{' '}
+				</Grid>{' '}
+			</Container>{' '}
+		</div>
+	);
+};
+
+export default Home;
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,76 +5,95 @@ import PostMessage from '../models/postMessage.js';
 
 const router = express.Router();
 
-export const getPosts = async (req, res) => { 
+export const getPosts = async(req, res) => {
     try {
         const postMessages = await PostMessage.find();
-                
+
         res.status(200).json(postMessages);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
-}
+};
 
-export const getPost = async (req, res) => { 
+export const getPost = async(req, res) => {
     const { id } = req.params;
 
     try {
         const post = await PostMessage.findById(id);
-        
+
         res.status(200).json(post);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
-}
+};
 
-export const createPost = async (req, res) => {
-    const { title, message, selectedFile, creator, tags } = req.body;
+export const createPost = async(req, res) => {
+    // const { title, message, selectedFile, creator, tags } = req.body;
+    const post = req.body;
 
-    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+    const newPostMessage = new PostMessage({
+        ...post,
+        creator: req.userId,
+        createdAt: new Date().toISOString(),
+    });
 
     try {
         await newPostMessage.save();
 
-        res.status(201).json(newPostMessage );
+        res.status(201).json(newPostMessage);
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
-}
+};
 
-export const updatePost = async (req, res) => {
+export const updatePost = async(req, res) => {
     const { id } = req.params;
     const { title, message, creator, selectedFile, tags } = req.body;
-    
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    if (!mongoose.Types.ObjectId.isValid(id))
+        return res.status(404).send(`No post with id: ${id}`);
 
     const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
 
     await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
 
     res.json(updatedPost);
-}
+};
 
-export const deletePost = async (req, res) => {
+export const deletePost = async(req, res) => {
     const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    if (!mongoose.Types.ObjectId.isValid(id))
+        return res.status(404).send(`No post with id: ${id}`);
 
     await PostMessage.findByIdAndRemove(id);
 
-    res.json({ message: "Post deleted successfully." });
-}
+    res.json({ message: 'Post deleted successfully.' });
+};
 
-export const likePost = async (req, res) => {
+export const likePost = async(req, res) => {
     const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
-    
+    if (!req.userId) return res.json({ message: 'Unauthenticated' });
+
+    if (!mongoose.Types.ObjectId.isValid(id))
+        return res.status(404).send(`No post with id: ${id}`);
+
     const post = await PostMessage.findById(id);
+    const index = post.likes.findIndex((id) => id === String(req.userId));
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
-    
+    if (index === -1) {
+        //like post
+        post.likes.push(req.userId);
+    } else {
+        //dislike a post
+        post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+        new: true,
+    });
+
     res.json(updatedPost);
-}
-
+};
 
 export default router;
